e2736a05b321095d4bf9c933fbd9be69472f58d4
Takashi Masuyama <mamewotoko@gmail.com>
2019-12-24 22:18:37 +0900

    add js graphic

--- ocaml/src/dance.ml
+++ ocaml/src/dance.ml
@@ -50,6 +50,12 @@ let name i =
   else "yyy"
 ;;
 
+(* let draw_caption outer_planet inner_planet orbits =
+ *   let outer_planet_name = name outer_planet in
+ *   let inner_planet_name = name inner_planet in
+ *   let orbit_text = Printf.sprintf "%.0f" orbits in
+ *   draw_text 10 10 *)
+
 let main () =
   let pi = 4.0 *. atan 1.0 in
   let canvas_len = 400 in
@@ -62,7 +68,6 @@ let main () =
   let outer_planet_radius = orbit outer_planet in
   let inner_planet_radius = orbit inner_planet in
   let interval_days = outer_planet_year /. 75. in
-  (* let ybottom = 40. in *)
   let ycenter = float_of_int (canvas_len / 2) in
   let xcenter = float_of_int (canvas_len / 2) in
   (* outer radius *)
@@ -74,23 +79,35 @@ let main () =
   let a1_interval = 2. *. pi *. interval_days /. outer_planet_year in
   let a2 = ref 0. in
   let a2_interval = 2. *. pi *. interval_days /. inner_planet_year in
+  (* wait until q key is pressed*)
   let rec interactive () =
     let event = Graphics.wait_next_event [Graphics.Key_pressed] in
     if event.key == 'q' then exit 0
     else interactive () in
   begin
     Graphics.open_graph (Printf.sprintf " %dx%d" canvas_len canvas_len);
+    (* draw text *)
+    let outer_planet_name = name outer_planet in
+    let inner_planet_name = name inner_planet in
+    let orbit_text = Printf.sprintf "%.0f orbits" orbits in
+    set_color Graphics.blue;
+    Graphics.moveto 10 (canvas_len - 20 - 5);
+    Graphics.draw_string outer_planet_name;
+    Graphics.moveto 10 (canvas_len - 40 - 5);
+    Graphics.draw_string inner_planet_name;
+    Graphics.moveto 10 20;
+    Graphics.draw_string orbit_text;
     while !r < rstop do
       let i = int_of_float (floor (!r /. interval_days /. 75.)) in
       let c = 
-        if i=0 then Graphics.black
-        else if i=1 then Graphics.blue
-        else if i=2 then Graphics.red
-        else if i=3 then Graphics.green
-        else if i=4 then Graphics.rgb 128 0 128
-        else if i=5 then Graphics.rgb 128 0 0
-        else if i=6 then Graphics.rgb 0 0 128
-        else if i=7 then Graphics.rgb 139 0 0
+        if i = 0 then Graphics.black
+        else if i = 1 then Graphics.blue
+        else if i = 2 then Graphics.red
+        else if i = 3 then Graphics.green
+        else if i = 4 then Graphics.rgb 128 0 128
+        else if i = 5 then Graphics.rgb 128 0 0
+        else if i = 6 then Graphics.rgb 0 0 128
+        else if i = 7 then Graphics.rgb 139 0 0
         else Graphics.rgb 255 165 0 in
       begin
         a1 := !a1 -. a1_interval;
@@ -100,8 +117,8 @@ let main () =
         let x2 = r2 *. cos !a2 in
         let y2 = r2 *. sin !a2 in
         Graphics.set_color c;
-        Graphics.moveto (int_of_float (x1 +. xcenter)) (int_of_float (y1 +. ycenter));
-        Graphics.lineto (int_of_float (x2 +. xcenter)) (int_of_float (y2 +. ycenter));
+        Graphics.moveto (int_of_float (x1 +. xcenter)) (int_of_float (~-. y1 +. ycenter));
+        Graphics.lineto (int_of_float (x2 +. xcenter)) (int_of_float (~-. y2 +. ycenter));
         r := !r +. interval_days;
       end
     done;

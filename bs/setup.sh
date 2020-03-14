efa8235ff539df2bfa941f9c27e3aebbd75774c1
Takashi Masuyama <mamewotoko@gmail.com>
2020-03-14 21:17:21 +0900

    add osx to ci

--- bs/setup.sh
+++ bs/setup.sh
@@ -1,2 +1,11 @@
 #! /bin/sh
+
+if [ $(uname) = Darwin ]; then
+    brew install nodejs make
+elif [ -f /etc/lsb-release ]; then
+    sudo apt-get update
+    sudo apt-get install -y nodejs make
+else
+    echo not yet
+fi
 npm i
--- python/setup.sh
+++ python/setup.sh
@@ -1,2 +1,12 @@
 #! /bin/sh
+
+if [ $(uname) = Darwin ]; then
+    brew install python3
+elif [ -f /etc/lsb-release ]; then
+    sudo apt-get update
+    sudo apt-get install -y python3
+else
+    echo not yet
+fi
+
 python3 -m pip install Pillow

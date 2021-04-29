a251c81a5d4d5cfb78462401a2cdd3b76c791d3f
Takashi Masuyama <mamewotoko@gmail.com>
2021-04-29 19:09:58 +0900

    add requried lib

--- lablgtk/setup.sh
+++ lablgtk/setup.sh
@@ -3,6 +3,7 @@ if [ $(uname) = Darwin ]; then
     brew install pkg-config
 elif [ -f /etc/lsb-release ]; then
     sudo apt-get update
+    sudo apt-get install -y make libexpat1-dev libgnomecanvas2-dev libgtk2.0-dev
 else
     echo not yet
 fi

943a71df8377426a0fa463d0c8850dd49f58a0b5
Takashi Masuyama <mamewotoko@gmail.com>
2021-05-02 17:45:30 +0900

    install xorgproto

--- ocaml/setup.sh
+++ ocaml/setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 if [ $(uname) = Darwin ]; then
     # brew install make cask
-    brew install xquartz xcb-xproto --cask
+    brew install xquartz xorgproto --cask
 elif [ -f /etc/lsb-release ]; then
     sudo apt-get update
     sudo apt-get install -y x11-app

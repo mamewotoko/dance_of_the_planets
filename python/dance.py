5ab6a01200af2a2905776256b62bed45baab3f06
Takashi Masuyama <mamewotoko@gmail.com>
2021-05-04 17:08:02 +0900

    fix issues reported by flake8

--- python/dance.py
+++ python/dance.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 from enum import Enum
 import math
 
@@ -12,7 +12,7 @@ maroon = (0x80, 0x00, 0x00)
 dark_red = (0x8B, 0x00, 0x00)
 navy = (0x8B, 0x00, 0x00)
 
-orange = (0xFF, 0xA5, 0X00)
+orange = (0xFF, 0xA5, 0x00)
 
 
 class Planets(Enum):
@@ -40,15 +40,15 @@ year = {
 }
 
 orbit = {
-  Planets.MERCURY: 57.91,
-  Planets.VENUS: 108.21,
-  Planets.EARTH: 149.60,
-  Planets.MARS: 227.92,
-  Planets.JUPITER: 778.57,
-  Planets.SATURN: 1433.5,
-  Planets.URANUS: 2872.46,
-  Planets.NEPTUNE: 4495.1,
-  Planets.PLUTO: 5869.7,
+    Planets.MERCURY: 57.91,
+    Planets.VENUS: 108.21,
+    Planets.EARTH: 149.60,
+    Planets.MARS: 227.92,
+    Planets.JUPITER: 778.57,
+    Planets.SATURN: 1433.5,
+    Planets.URANUS: 2872.46,
+    Planets.NEPTUNE: 4495.1,
+    Planets.PLUTO: 5869.7,
 }
 line_color = [
     black,
@@ -69,10 +69,8 @@ canvas_len = 400
 def draw_line(draw, color, from_x, from_y, to_x, to_y):
     draw.line((from_x, from_y, to_x, to_y), fill=color, width=1)
 
-#font = ImageFont.truetype("/Library/Fonts/Comic Sans MS.ttf", 14)
 
 def draw_text(draw, color, x, y, text):
-#    draw.text((x, y), text, fill=color, font=font)
     draw.text((x, y), text, fill=color)
 
 
@@ -86,7 +84,7 @@ def main(draw, outer_planet, inner_planet, orbits):
     xcenter = canvas_len / 2
     r1 = ycenter
     r2 = r1 * inner_planet_radius / outer_planet_radius
-    r = 0.0 # theta
+    r = 0.0
     a1 = 0.0
     a2 = 0.0
     rstop = outer_planet_year * orbits
@@ -99,9 +97,9 @@ def main(draw, outer_planet, inner_planet, orbits):
     draw_text(draw, blue, 10, 10, outer_planet_name)
     draw_text(draw, blue, 10, 30, inner_planet_name)
     draw_text(draw, blue, 10, (canvas_len - 30), orbit_text)
-    
+
     while r < rstop:
-        i = int(r / interval_days / 75.)
+        i = int(r / interval_days / 75.0)
         color = line_color[i]
         a1 = a1 - a1_interval
         a2 = a2 - a2_interval
@@ -111,10 +109,10 @@ def main(draw, outer_planet, inner_planet, orbits):
         y2 = r2 * math.sin(a2)
         draw_line(draw, color, xcenter + x1, ycenter + y1, xcenter + x2, ycenter + y2)
         r = r + interval_days
-        
 
-if __name__ == '__main__':
-    im = Image.new('RGB', (400, 400), (0xFF, 0xFF, 0xFF))
+
+if __name__ == "__main__":
+    im = Image.new("RGB", (400, 400), (0xFF, 0xFF, 0xFF))
     draw = ImageDraw.Draw(im)
     main(draw, Planets.EARTH, Planets.VENUS, 8.0)
-    im.save('result_daonce.png')
+    im.save("result_daonce.png")

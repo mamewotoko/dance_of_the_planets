2f21d497b126ac42d058d797e833ea17339479a7
Takashi Masuyama <mamewotoko@gmail.com>
2021-05-29 19:58:39 +0900

    draw muliple paths

--- haskell/src/main.hs
+++ haskell/src/main.hs
@@ -6,16 +6,17 @@ import Codec.Picture
 import Graphics.Rasterific
 import Graphics.Rasterific.Texture
 
-drawTriangle :: IO ()
-drawTriangle =
+drawTriangles :: IO ()
+drawTriangles =
   writePng "triangle.png" image
   where
     image =
-      renderDrawing 400 400 black $
-      withTexture (uniformTexture white) $
-      stroke 2 (JoinMiter 0)
-      (CapStraight 0, CapStraight 0)
-      triangle
+      renderDrawing 400 400 black $ do
+      mapM_ drawTriangle [(white, triangle), (red, triangle2), (green, triangle3)]
+
+    drawTriangle (color, path) =
+      withTexture (uniformTexture color) $ 
+      stroke 2 (JoinMiter 0) (CapStraight 0, CapStraight 0) path
 
     triangle =
         Path (V2 200 50) True
@@ -23,10 +24,26 @@ drawTriangle =
           , PathLineTo (V2 350 350)
           ]
 
+    triangle2 =
+        Path (V2 200 0) True
+          [ PathLineTo (V2 50 400)
+          , PathLineTo (V2 350 400)
+          ]
+
+    triangle3 =
+        Path (V2 0 200) True
+          [ PathLineTo (V2 400 50)
+          , PathLineTo (V2 400 350)
+          ]
+
     black = PixelRGBA8 0 0 0 255
 
     white = PixelRGBA8 255 255 255 255
 
+    red = PixelRGBA8 255 0 0 255
+
+    green = PixelRGBA8 0 0 255 255
+
 main :: IO ()
 main = do
-  drawTriangle
+  drawTriangles

a02731303739570500b2238c5f1288014720ed73
Takashi Masuyama <mamewotoko@gmail.com>
2021-07-11 11:11:26 +0900

    update sbt install command

--- scalajs/setup.sh
+++ scalajs/setup.sh
@@ -4,7 +4,12 @@ if [ $(uname) = Darwin ]; then
     brew install sbt
 elif [ -f /etc/lsb-release ]; then
     echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-    curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
     sudo apt-get update
-    sudo apt install sbt openjdk-11-jdk
+    sudo apt-get install sbt openjdk-11-jdk
+
+    # echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+    # curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+    # sudo apt-get update
+    # sudo apt install sbt openjdk-11-jdk
 fi

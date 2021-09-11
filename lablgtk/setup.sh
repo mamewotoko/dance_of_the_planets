8e1e1d0c5e0ec683bfca37247ab6b654929bb03f
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 19:40:28 +0900

    fix build.sh

--- lablgtk/setup.sh
+++ lablgtk/setup.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
+set -e
 if [ $(uname) = Darwin ]; then
     brew install gtk+ libgnomecanvas expat
 elif [ -f /etc/lsb-release ]; then
     sudo apt update
-    sudo apt install -y gtk2.0 libgnomecanvas2-dev libexpat1-dev libgtk2.0-dev
+    sudo apt install -y libgnomecanvas2-dev libexpat1-dev libgtk2.0-dev
 else
     echo not supported yet
 fi

40883f4c2e2ab017353d2dc967e4ca8d0a4e0fe9
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 17:11:33 +0900

    add sudo

--- scalajs/setup.sh
+++ scalajs/setup.sh
@@ -6,7 +6,7 @@ elif [ -f /etc/lsb-release ]; then
     sudo apt-get install -y gnupg2 curl default-jre
     echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
     curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 sudo apt-key add -
-    apt-get update
+    sudo apt-get update
     sudo apt-get install sbt
 
     # echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list

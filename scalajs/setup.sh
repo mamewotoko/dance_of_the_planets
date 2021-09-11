350874280e8e26af3d13b83fe4098e9bf165462a
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 15:27:24 +0900

    fix sbt install script

--- scalajs/setup.sh
+++ scalajs/setup.sh
@@ -3,13 +3,14 @@
 if [ $(uname) = Darwin ]; then
     brew install sbt
 elif [ -f /etc/lsb-release ]; then
-    echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-    sudo apt-get update
-    sudo apt-get install sbt openjdk-11-jdk
+    apt-get install -y gnupg2 curl default-jre
+    echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+    curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
+    apt-get update
+    apt-get install sbt
 
     # echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-    # curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+    # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
     # sudo apt-get update
-    # sudo apt install sbt openjdk-11-jdk
+    # sudo apt-get install sbt openjdk-11-jdk
 fi

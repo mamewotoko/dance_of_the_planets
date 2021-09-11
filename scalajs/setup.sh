34a26618be94870d0f7c488c83aa726607babdc8
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 17:38:19 +0900

    install libs

--- scalajs/setup.sh
+++ scalajs/setup.sh
@@ -7,7 +7,7 @@ elif [ -f /etc/lsb-release ]; then
     echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
     curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 sudo apt-key add -
     sudo apt-get update
-    sudo apt-get install sbt
+    sudo apt-get install -y sbt
 
     # echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
     # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823

fc38b815fce03045343e82e3fb320d320fe1f276
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 15:38:33 +0900

    install ext library

--- ocaml/setup.sh
+++ ocaml/setup.sh
@@ -2,7 +2,7 @@
 if [ $(uname) = Darwin ]; then
     # brew install make cask
     brew install xorgproto
-    brew install xquartz --cask
+    brew cask install xquartz
 elif [ -f /etc/lsb-release ]; then
     sudo apt-get update
     sudo apt-get install -y x11-app

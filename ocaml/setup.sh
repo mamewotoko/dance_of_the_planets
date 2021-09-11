8e6e131b8d6514c4fb159449f25b419850e28c64
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 16:56:26 +0900

    install libs

--- ocaml/setup.sh
+++ ocaml/setup.sh
@@ -2,7 +2,7 @@
 if [ $(uname) = Darwin ]; then
     # brew install make cask
     brew install xorgproto
-    brew cask install xquartz
+    brew install xquartz --cask
 elif [ -f /etc/lsb-release ]; then
     sudo apt-get update
     sudo apt-get install -y x11-app

eb8fc5f830d6cc6da1a2c72db018b7459a4d35c5
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 20:17:26 +0900

    add error exit

--- lablgtk/setup.sh
+++ lablgtk/setup.sh
@@ -7,6 +7,7 @@ elif [ -f /etc/lsb-release ]; then
     sudo apt install -y libgnomecanvas2-dev libexpat1-dev libgtk2.0-dev
 else
     echo not supported yet
+    exit 1
 fi
 #opam install -y depext
 #opam depext -y lablgtk conf-gnomecanvas dune

ef03f1bdf73a6cc02279f8bb1a5e6463d70a7206
Takashi Masuyama <mamewotoko@gmail.com>
2022-04-03 13:51:43 +0900

    install lib

---
+++ rust/setup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -e
+if [ $(uname) = Darwin ]; then
+    brew install gtk+ pango pkg-config
+elif [ -f /etc/lsb-release ]; then
+    sudo apt-get update
+    sudo apt-get install -y cargo pkg-config libgtk-3-dev
+else
+    echo not supported yet
+    exit 1
+fi

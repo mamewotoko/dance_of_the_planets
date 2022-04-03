ac1a6f217c0aabb31c267d0c203eacaf088aea7f
Takashi Masuyama <mamewotoko@gmail.com>
2022-04-03 15:48:14 +0900

    skip installing xorg

--- ocaml/setup.sh
+++ ocaml/setup.sh
@@ -2,8 +2,9 @@
 set -e
 if [ $(uname) = Darwin ]; then
     # brew install make cask
-    brew install xorgproto
-    brew install xquartz --cask
+    # brew install xorgproto
+    # brew install xquartz --cask
+    echo nothing to do
 elif [ -f /etc/lsb-release ]; then
     sudo apt-get update
     sudo apt-get install -y x11-apps
